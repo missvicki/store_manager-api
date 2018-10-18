86c511e1a8fab10015642aba5193c803f21cd472
Victor Nomwesigwa <31304859+missvicki@users.noreply.github.com>
2018-10-18 16:59:40 +0300

    Update test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,23 +7,18 @@ import json
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-from api import app
+from api.app import PRODUCTS, app
 
 BASE_URL_PRODUCTS = 'http://127.0.0.1:5000/storemanager/api/v1.0/products'
 BAD_ITEM_URL_PRODUCTS = '{}/16'.format(BASE_URL_PRODUCTS)
 GOOD_ITEM_URL_PRODUCTS = '{}/10'.format(BASE_URL_PRODUCTS)
 
-BASE_URL_SALES = 'http://127.0.0.1:5000/storemanager/api/v1.0/sales'
-BAD_ITEM_URL_SALES = '{}/4'.format(BASE_URL_SALES)
-GOOD_ITEM_URL_SALES = '{}/3'.format(BASE_URL_SALES)
-
 class TestStoreManagerApi(unittest.TestCase):
     """TestStoreManagerApi(unittest.TestCase)--holds all tests we shall perform"""
     def setUp(self):
         """setUp(self)---"""
-        self.backup_products = deepcopy(app.PRODUCTS)
-        self.backup_sales = deepcopy(app.SALES)
-        self.app = app.app.test_client()
+        self.backup_products = deepcopy(PRODUCTS)
+        self.app = app.test_client()
         self.app.testing = True
 
     def test_get_all_products(self):
@@ -33,14 +28,6 @@ class TestStoreManagerApi(unittest.TestCase):
         print(data_products)
         self.assertEqual(response_products.status_code, 200, msg="Found Products")
         self.assertEqual(len(data_products['products']), 12)
-
-    def test_get_all_sales(self):
-        """test_get_all_sales(self)---"""
-        response_sales = self.app.get(BASE_URL_SALES)
-        data_sales = json.loads(response_sales.get_data())
-        print(data_sales)
-        self.assertEqual(response_sales.status_code, 200, msg="Found Sales")
-        self.assertEqual(len(data_sales['sales']), 3)
 
     def test_get_one_product(self):
         """test__get_one_product(self)---"""
@@ -54,11 +41,6 @@ class TestStoreManagerApi(unittest.TestCase):
         """test_product_not_exist(self) --"""
         response_product = self.app.get(BAD_ITEM_URL_PRODUCTS)
         self.assertEqual(response_product.status_code, 404, msg="Didn't find product")
-
-    def test_sale_not_exist(self):
-        """test_sale_not_exist(self) --"""
-        response_sale = self.app.get(BAD_ITEM_URL_SALES)
-        self.assertEqual(response_sale.status_code, 404, msg="Didn't find sale")
 
     def test_post_product(self):
         """test_post_product(self)"""
@@ -85,7 +67,6 @@ class TestStoreManagerApi(unittest.TestCase):
         """tearDown(self)---"""
         # reset app.products to initial state
         app.PRODUCTS = self.backup_products
-        app.SALE = self.backup_sales
     
 if __name__ == "__main__":
     unittest.main()
