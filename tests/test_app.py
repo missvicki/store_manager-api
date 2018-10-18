80d201bad5b29d5a3cef4602ef1cba8f2432e942
Victor Nomwesigwa <31304859+missvicki@users.noreply.github.com>
2018-10-18 14:23:41 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,7 @@ import json
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-from api.app import app
+from app import app
 
 BASE_URL_PRODUCTS = 'http://127.0.0.1:5000/storemanager/api/v1.0/products'
 BAD_ITEM_URL_PRODUCTS = '{}/16'.format(BASE_URL_PRODUCTS)
