dc437db28161d1aad14c8979500b1af940088779
Victor Nomwesigwa <31304859+missvicki@users.noreply.github.com>
2018-10-19 08:49:03 +0300

    [#161204536]---v1

--- api/app.py
+++ api/app.py
@@ -134,19 +134,19 @@ def _record_exists_(productid):
     """_record_exists(productid) returns a product in products via product_id"""
     return [product for product in PRODUCTS if product["product_id"] == productid]
 
-@app.route('/storemanager/api/v1.0')
+@app.route('/storemanager/api/v1')
 def hello():
     """my first home"""
     return 'Hello Welcome to Store Manager API'
 
 #get all products
-@app.route('/storemanager/api/v1.0/products', methods=['GET'])
+@app.route('/storemanager/api/v1/products', methods=['GET'])
 def get_products():
     """get_products() -- returns all products"""
     return jsonify({'products': PRODUCTS})
 
 #get specific product
-@app.route('/storemanager/api/v1.0/products/<int:_id>', methods=['GET'])
+@app.route('/storemanager/api/v1/products/<int:_id>', methods=['GET'])
 def get_product(_id):
     """get_product(_id) -- returns a product via its id"""
     _product_ = _get_product(_id)
@@ -155,7 +155,7 @@ def get_product(_id):
     return jsonify({'products': _product_})
 
 #post a product
-@app.route('/storemanager/api/v1.0/products', methods=['POST'])
+@app.route('/storemanager/api/v1/products', methods=['POST'])
 def create_product():
     """create_product() --returns a product that has been added"""
     prod_id = request.get_json('product_id')
