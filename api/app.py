ae7d286330c041396112610a4db5a5a0602804df
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-10-24 21:13:26 +0300

    edit routes and get products

--- api/app.py
+++ api/app.py
@@ -125,14 +125,21 @@ def bad_request(error):
 @app.route('/')
 def hello():
     """my first home"""
-    return 'Hello Welcome to Store Manager API'
+    return "Hello Welcome to Store Manager API"
 
-#get all products
-@app.route('/storemanager/api/v1/products', methods=['GET'])
-def get_products():
-    """get_products() -- returns all products"""
-    return jsonify({'products': PRODUCTS})
+#other methods
+def _get_product(productid):
+    """_get_product(productid) returns a product in products via product_id"""
+    return [product for product in PRODUCTS if product['product_id'] == productid]
 
+#get all products and post a product
+@app.route('/api/v1/products', methods=['GET'])
+def products():
+    """returns all products"""
+    if request.method == 'GET':
+        return jsonify({'products': PRODUCTS})
+    else:
+        return jsonify({"Invalid": "Method"})
 
 if __name__ == '__main__':
     app.run(debug=True)
