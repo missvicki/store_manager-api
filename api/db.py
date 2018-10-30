6c812e54706417d3fba749239b1d2ea9ec5dfb6e
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-10-30 16:10:51 +0300

    [#161584920] create user and get users

--- api/db.py
+++ api/db.py
@@ -1,29 +1,31 @@
 """database models"""
 class Products:
     """product model"""
-    def __init__(self, product_name, category, unit_price, quantity, measure, date):
+    def __init__(self, product_name, category, unit_price, quantity, measure):
         self.product_name = product_name
         self.category = category
         self.unit_price = unit_price
         self.quantity = quantity
         self.measure = measure
-        self.date = date
 
 class Sales:
     """sale model"""
-    def __init__(self, product_id, user_id, quantity, total, date):
-        self.product_id = product_id
+    def __init__(self, user_id):
         self.user_id = user_id
-        self.quantity = quantity
-        self.total = total
-        self.date = date
 
+class SalesHasProducts:
+    """sales has products"""
+    def __init__(self, sale_id, product_id, quantity, total):
+        self.sale_id = sale_id
+        self.product_id = product_id
+        self.quantity =quantity
+        self.total = total
+        
 class Users:
     """user model"""
-    def __init__(self, name, user_name, password, role, date):
+    def __init__(self, name, user_name, password, role):
         # self.user_id = user_id
         self.name = name
         self.user_name = user_name
         self.password = password
         self.role = role
-        self.date = date
