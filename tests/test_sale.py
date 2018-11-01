e3ede534b82dd8a8ce5e7fc9e407be293394b898
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-01 16:36:55 +0300

    remove

--- tests/test_sale.py
+++
@@ -1,9 +0,0 @@
-"""test authentication"""
-import unittest
-import json
-import psycopg2
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-from api.views import app
-from api.database import DatabaseConnection
