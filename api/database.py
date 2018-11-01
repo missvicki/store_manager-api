f9d2fb78880246d1ead60f4831093a67e2f3478c
Victor Nomwesigwa <nvbethany479@gmail.com>
2018-11-01 16:04:35 +0300

    format

--- api/database.py
+++ api/database.py
@@ -8,6 +8,7 @@ from config import env_config
 class DatabaseConnection:
     """Connect to the database"""
     def __init__(self):
+
         try:
             if env_config['testing'] == True:
                 self.connection = psycopg2.connect(database='storemanager_test_db', user='postgres', password='admin', host='localhost', port='5432')
@@ -19,15 +20,20 @@ class DatabaseConnection:
                 self.connection.autocommit = True
                 # allow you to read from and write to database
                 self.cursor = self.connection.cursor()
+
         except psycopg2.DatabaseError as anything:
             print (anything)
+
     def drop_tables(self):
         """drop tables if exist"""
+
         self.cursor.execute(
             "DROP TABLE IF EXISTS products, users, sales, sales_has_products, login CASCADE"
         )
+
     def create_tables(self):
-        """create product table"""    
+        """create product table""" 
+
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS products (
@@ -42,6 +48,7 @@ class DatabaseConnection:
                 delete_status BOOLEAN DEFAULT FALSE);
             """
         )
+
         """create user table"""  
         self.cursor.execute(
             """
@@ -56,6 +63,7 @@ class DatabaseConnection:
                 delete_status BOOLEAN DEFAULT FALSE);
             """
         )
+
         """create sales table"""  
         self.cursor.execute(
             """
@@ -70,6 +78,7 @@ class DatabaseConnection:
                     ON UPDATE CASCADE);
             """
         )
+
         """create sales has products table"""
         self.cursor.execute(
             """
@@ -88,6 +97,7 @@ class DatabaseConnection:
             );
             """
         )
+
         """login table create"""
         self.cursor.execute(
             """
@@ -103,6 +113,7 @@ class DatabaseConnection:
         
     def insert_data_products(self, data):
         """inserts values into table products"""
+
         try:
             self.cursor.execute(
                 """
@@ -112,8 +123,10 @@ class DatabaseConnection:
             )
         except:
             return False
+
     def check_product_exists_name(self, product_name):
         """check if product exists"""
+
         try:
             self.cursor.execute(
                 "SELECT * FROM products WHERE product_name = '{}' AND delete_status = FALSE" .format(product_name)
@@ -121,8 +134,10 @@ class DatabaseConnection:
             return self.cursor.fetchone()
         except:
             return False
+
     def getProducts(self):
         """get all products"""
+
         try:
             self.cursor.execute(
                 "SELECT * FROM products WHERE delete_status = FALSE"
@@ -130,159 +145,242 @@ class DatabaseConnection:
             _products = self.cursor.fetchall()
             for product in _products:
                 return ("products: {0}".format(product))
+
         except:
             return False
 
     def getoneProduct(self, _pid):
         """get one product"""
+
         try:
             self.cursor.execute(
                 "SELECT * FROM products WHERE product_id = %s AND delete_status = FALSE", [_pid]
             )
             _products = self.cursor.fetchall()
+
             if _products:
                 for products in _products:
                     return ("product: {0}".format(products)) 
             else:
                 return ("no product with that id")
+
         except:
             return False
+
     def deloneProduct(self, _pid):
         """delete one product"""
-        self.cursor.execute(
-            # "DELETE FROM products WHERE product_id = %s", [_pid]
-            "UPDATE products SET delete_status=TRUE , date_modified =CURRENT_TIMESTAMP WHERE product_id = {}".format(_pid
+        try:
+            self.cursor.execute(
+                # "DELETE FROM products WHERE product_id = %s", [_pid]
+                "UPDATE products SET delete_status=TRUE , date_modified =CURRENT_TIMESTAMP WHERE product_id = {}".format(_pid
+                )
             )
-        )
+        except:
+            return False
 
     def check_product_exists_id(self, product_id):
         """check if product exists"""
-        self.cursor.execute(
-            "SELECT * FROM products WHERE product_id = %s AND delete_status= FALSE", [product_id]) 
-        return self.cursor.fetchone()  
+
+        try:
+            self.cursor.execute(
+                "SELECT * FROM products WHERE product_id = %s AND delete_status= FALSE", [product_id]) 
+            return self.cursor.fetchone()
+
+        except:
+            return False
 
     def modify_product(self, product_name, category, unit_price, quantity, measure,product_id):
         """modify product"""
-        self.cursor.execute(
-            "UPDATE products SET product_name='{}', category='{}', \
-            unit_price={}, quantity={}, measure = '{}', date_modified=CURRENT_TIMESTAMP\
-            WHERE product_id = {} AND delete_status = FALSE"
-            .format(product_name, category, unit_price, quantity, measure, product_id)
-        ) 
+        try:
+            self.cursor.execute(
+                "UPDATE products SET product_name='{}', category='{}', \
+                unit_price={}, quantity={}, measure = '{}', date_modified=CURRENT_TIMESTAMP\
+                WHERE product_id = {} AND delete_status = FALSE"
+                .format(product_name, category, unit_price, quantity, measure, product_id)
+            )
+
+        except:
+            return False
     
     def insert_table_users(self, record):
         """add data to table users"""
-        self.cursor.execute(
-            """
-            INSERT INTO users(name, user_name, password, role) \
-            VALUES('{}', '{}', '{}', '{}')
-            """.format(record.name, record.user_name, record.password, record.role)
-        )
+
+        try:
+            self.cursor.execute(
+                """
+                INSERT INTO users(name, user_name, password, role) \
+                VALUES('{}', '{}', '{}', '{}')
+                """.format(record.name, record.user_name, record.password, record.role)
+            )
+        
+        except:
+            return False
 
     def default_admin(self):
         """inserts default admin"""
-        self.cursor.execute(
-            """
-            INSERT INTO users(name, user_name, password, role)\
-            VALUES('Vicki', 'vickib', 'vibel', 'admin');
-            """
-        )
+
+        try:
+            self.cursor.execute(
+                """
+                INSERT INTO users(name, user_name, password, role)\
+                VALUES('Vicki', 'vickib', 'vibel', 'admin');
+                """
+            )
+        
+        except:
+            return False
 
     def insert_table_login(self, record):
         """add data to table login"""
-        self.cursor.execute(
-            """
-            INSERT INTO login(user_name, password, role) \
-            VALUES('{}', '{}', '{}')
-            """.format(record.user_name, record.password, record.role)
-        )
+
+        try:
+            self.cursor.execute(
+                """
+                INSERT INTO login(user_name, password, role) \
+                VALUES('{}', '{}', '{}')
+                """.format(record.user_name, record.password, record.role)
+            )
+        
+        except:
+            return False
 
     def getUsers(self):
         """get all users"""
-        self.cursor.execute(
-            "SELECT * FROM users WHERE delete_status= FALSE"
-        )
-        _users = self.cursor.fetchall()
-        for user in _users:
-            return ("users: {0}" .format(user))
+        try:
+            self.cursor.execute(
+                "SELECT * FROM users WHERE delete_status= FALSE"
+            )
+            _users = self.cursor.fetchall()
+            for user in _users:
+                return ("users: {0}" .format(user))
+        except:
+            return False
 
     def check_user_exists(self, user_name, password, role):
         """check if user exists"""
-        self.cursor.execute(
-            "SELECT * FROM users WHERE user_name = '{}' AND password = '{}' AND role = '{}' AND delete_status= FALSE" .format(user_name, password, role)
-        )
-        return self.cursor.fetchone()
+
+        try:
+            self.cursor.execute(
+                "SELECT * FROM users WHERE user_name = '{}' AND password = '{}' AND role = '{}' AND delete_status= FALSE" .format(user_name, password, role)
+            )
+            return self.cursor.fetchone()
+        
+        except:
+            return False
 
     def getoneUser(self, _uid):
         """get one user"""
-        self.cursor.execute(
-            "SELECT * FROM users WHERE user_id = %s AND delete_status= FALSE", [_uid]
-        )
-        _users = self.cursor.fetchall()
-        if _users:
-            for user in _users:
-                return ("user: {0}".format(user)) 
-        else:
-            return ("no user with that id")
+
+        try:
+            self.cursor.execute(
+                "SELECT * FROM users WHERE user_id = %s AND delete_status= FALSE", [_uid]
+            )
+            _users = self.cursor.fetchall()
+            if _users:
+                for user in _users:
+                    return ("user: {0}".format(user)) 
+            else:
+                return ("no user with that id")
+
+        except:
+            return False
 
     def deloneuser(self, _uid):
         """delete one user"""
-        self.cursor.execute(
-            # "DELETE FROM users WHERE user_id = %s", [_uid]
-            "UPDATE users SET delete_status=TRUE, date_modified= CURRENT_TIMESTAMP WHERE user_id = {}".format(_uid)
-        )
+
+        try:
+            self.cursor.execute(
+                # "DELETE FROM users WHERE user_id = %s", [_uid]
+                "UPDATE users SET delete_status=TRUE, date_modified= CURRENT_TIMESTAMP WHERE user_id = {}".format(_uid)
+            )
+        except:
+            return False
 
     def check_user_exists_id(self, user_id):
         """check if user exists"""
-        self.cursor.execute(
-            "SELECT * FROM users WHERE user_id = %s AND delete_status= FALSE", [user_id]) 
-        return self.cursor.fetchone()  
+
+        try:
+            self.cursor.execute(
+                "SELECT * FROM users WHERE user_id = %s AND delete_status= FALSE", [user_id]) 
+            return self.cursor.fetchone()
+
+        except:
+            return False
 
     def insert_data_sales(self, data):
         """insert data into sales table"""
-        self.cursor.execute("INSERT INTO sales(user_id) VALUES({}) RETURNING sale_id".format(data.user_id)
-        )
-        return self.cursor.fetchone()[0]
+
+        try:
+            self.cursor.execute("INSERT INTO sales(user_id) VALUES({}) RETURNING sale_id".format(data.user_id)
+            )
+            return self.cursor.fetchone()[0]
+        
+        except:
+            return False
 
     def insert_data_sales_has_products(self, data):
         """insert data into salesproducts table"""
-        self.cursor.execute(
-            """
-            INSERT INTO sales_has_products(sale_id, product_id, quantity, total) \
-            VALUES({}, {}, {}, {})
-            """.format(data.sale_id, data.product_id, data.quantity, data.total)
-        )
+
+        try:
+            self.cursor.execute(
+                """
+                INSERT INTO sales_has_products(sale_id, product_id, quantity, total) \
+                VALUES({}, {}, {}, {})
+                """.format(data.sale_id, data.product_id, data.quantity, data.total)
+            )
+        
+        except:
+            return False
         
     def getsales(self):
         """get one sale"""
-        self.cursor.execute(
-            "SELECT sales.sale_id, sales.user_id, products.product_id, \
-            sales_has_products.total, sales_has_products.quantity, sales.date_created, sales.date_modified \
-            FROM sales_has_products, sales, products WHERE sales.sale_id = sales_has_products.sale_id \
-            AND products.product_id = sales_has_products.product_id" 
-        )
-        _sale = self.cursor.fetchall()
-        for sale in _sale:
-            return ("sales: {0}" .format(sale))
+
+        try:
+            self.cursor.execute(
+                "SELECT sales.sale_id, sales.user_id, products.product_id, \
+                sales_has_products.total, sales_has_products.quantity, sales.date_created, sales.date_modified \
+                FROM sales_has_products, sales, products WHERE sales.sale_id = sales_has_products.sale_id \
+                AND products.product_id = sales_has_products.product_id" 
+            )
+            _sale = self.cursor.fetchall()
+            for sale in _sale:
+                return ("sales: {0}" .format(sale))
+        
+        except:
+            return False
 
     def getQuantity(self, id_):
         """get qty"""
-        self.cursor.execute(
-            "SELECT quantity FROM products WHERE product_id = %s AND delete_status= FALSE", [id_]
-        )
-        return self.cursor.fetchone()[0]
+
+        try:
+            self.cursor.execute(
+                "SELECT quantity FROM products WHERE product_id = %s AND delete_status= FALSE", [id_]
+            )
+            return self.cursor.fetchone()[0]
+        
+        except:
+            return False
 
     def getPrice(self, id_):
         """get price"""
-        self.cursor.execute(
-            "SELECT unit_price FROM products WHERE product_id = %s AND delete_status= FALSE", [id_]
-        )
-        return self.cursor.fetchone()[0]
+
+        try:
+            self.cursor.execute(
+                "SELECT unit_price FROM products WHERE product_id = %s AND delete_status= FALSE", [id_]
+            )
+            return self.cursor.fetchone()[0]
+        
+        except:
+            return False
 
     def updateProductqty(self, qty, pdtid):
         """update pdt qty"""
-        self.cursor.execute(
-            "UPDATE products SET quantity={}, date_modified=CURRENT_TIMESTAMP WHERE product_id = {} \
-            AND delete_status=False".format(qty, pdtid)
-        )
 
+        try:
+            self.cursor.execute(
+                "UPDATE products SET quantity={}, date_modified=CURRENT_TIMESTAMP WHERE product_id = {} \
+                AND delete_status=False".format(qty, pdtid)
+            )
+        
+        except:
+            return False
